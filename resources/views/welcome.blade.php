7409d13ce347c256aff6cf2ec54b3154e1c78853
Jake Och <ger.minationstudio@gmail.com>
2018-08-24 10:04:44 -0400

    Update Images Location

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,11 +13,11 @@
             <p>RaffleJar allows you to run as many contests as you'd like. </p>
         </div>
         <div class="col-sm-8">
-            <img src="http://blog.germinationstudio.com/storage/images/0e34bf6ba0e73b2fdab049e6a4523a1f.png" class="img-responsive img-thumbnail" alt="">
+            <img src="/storage/images/0e34bf6ba0e73b2fdab049e6a4523a1f.png" class="img-responsive img-thumbnail" alt="">
         </div>
     </div>
     <div class="row margin-top-40">
-        <div class="col-sm-8"><img src="http://blog.germinationstudio.com/storage/images/8b937218cb581f38e87530c1d326f79c.png" class="img-responsive img-thumbnail" alt=""></div>
+        <div class="col-sm-8"><img src="/storage/images/8b937218cb581f38e87530c1d326f79c.png" class="img-responsive img-thumbnail" alt=""></div>
         <div class="col-sm-4"><h4>Easy to add tickets!</h4>
         <p>You can easily add tikets to your contest from any device! Add multiple tikets for the same contestant with one click!</p></div>
     </div>
@@ -26,10 +26,10 @@
             <h4>Add Prizes and Give the Donors Credit</h4>
             <p>Draw your winners with a single click, keep track of who donates the prizes!</p>
         </div>
-        <div class="col-sm-8"><img src="http://blog.germinationstudio.com/storage/images/ceeeaaaaad593d55f23df1f62a999b4a.png" class="img-thumbnail img-responsive" alt=""></div>
+        <div class="col-sm-8"><img src="/storage/images/ceeeaaaaad593d55f23df1f62a999b4a.png" class="img-thumbnail img-responsive" alt=""></div>
     </div>
     <div class="row margin-top-40">
-        <div class="col-sm-8"><img src="http://blog.germinationstudio.com/storage/images/bc273d0c04e348a10b44f9654cb73958.png" class="img-responsive img-thumbnail" alt=""></div>
+        <div class="col-sm-8"><img src="/storage/images/bc273d0c04e348a10b44f9654cb73958.png" class="img-responsive img-thumbnail" alt=""></div>
         <div class="col-sm-4"><h4>Contacting Winners is Easy</h4>
         <p>Keep track of how you contact the winners, and who has claimed their prizes!</p></div>
     </div>
